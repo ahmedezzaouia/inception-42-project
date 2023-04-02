996f808081f8bf080b22dd536624cf683ee1dcde
ahmedezzaouia <professional.world2@gmail.com>
2023-04-02 02:06:15 +0000

    refactor wordpress

--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -8,14 +8,29 @@ cd /var/www/html/wordpress
 wp core download --allow-root
 
 # Creating the wp-config.php file using this command.
-wp core config --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --allow-root
+wp core config \
+    --dbname=$DB_NAME \
+    --dbuser=$DB_USER \
+    --dbpass=$DB_PASS \
+    --dbhost=$DB_HOST \
+    --allow-root
 
 # Installing Wordpress using the given environment variables to avoid showing the Wordpress installation page every time we run the container
-wp core install --url=$WORDPRESS_DOMAIN --title=$WORDPRESS_SITE_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
+wp core install \
+    --url=$WORDPRESS_DOMAIN \
+    --title=$WORDPRESS_SITE_TITLE \
+    --admin_user=$WORDPRESS_ADMIN_USER \
+    --admin_password=$WORDPRESS_ADMIN_PASSWORD \
+    --admin_email=$WORDPRESS_ADMIN_EMAIL \
+    --allow-root
 
 # Create another user
-wp user create $WORDPRESS_USER_LOGIN $WORDPRESS_USER_MAIL --role=author --user_pass=$WORDPRESS_USER_PASS --allow-root
-
+wp user create \
+    $WORDPRESS_USER_LOGIN \
+    $WORDPRESS_USER_MAIL \
+    --role=author \
+    --user_pass=$WORDPRESS_USER_PASS \
+    --allow-root
 
 # Start PHP-FPM to read and execute wordpress code 
 /usr/sbin/php-fpm7.3 -F
